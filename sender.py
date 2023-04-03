d1718c1a5f29c849f8614e9020cf95ff01addca2
BORUSAMA <solodovnikovmoris@gmail.com>
2023-04-03 21:27:18 +0200

    Et pourquoi pas du Victor Hugo

--- sender.py
+++ sender.py
@@ -1,10 +1,11 @@
 import os,sys,subprocess
 
-if sys.argv[1]=="/":
-    result = subprocess.run(["ls","-l",sys.argv[1]])
+if sys.argv[1][-1]=="/":
+    result = subprocess.run(["ls",sys.argv[1]],capture_output=True)
+    liste=result.stdout.decode().split()
 else:
     result = subprocess.run(["ls",sys.argv[1]],capture_output=True)
-    #result=result+subprocess.run(["ls","-d","-l",sys.argv[1]])
+    liste=[sys.argv[1]]
+    liste.append(result.stdout.decode().split())
 
-print(result.stdout)
-print("test")
+print(liste)
